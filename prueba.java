261589c8d404369bebfb21af765acb39c452d9e8
Carol <krolduarte@gmail.com>
2021-10-13 11:09:30 +0200

    Agregando localidad y fecha

--- prueba.java
+++ prueba.java
@@ -2,7 +2,8 @@ public class MiPrimeraClase {
 
 	public static void main(String args[]) {
 	
-	System.out.println("Carol Andrea Duarte Higuera");	
+	System.out.println("Carol Andrea Duarte Higuera");
+	System.out.println("Bogota 1984");	
 	
 
 	}
